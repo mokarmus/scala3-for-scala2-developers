d5b5a110d5f18b6000515a70935ee8209cc0fa72
mokarmus <mateusz.okarmus.ext@iceservices.com>
2023-07-11 22:02:08 +0200

    extensions

--- src/main/scala/03-extensions.scala
+++ src/main/scala/03-extensions.scala
@@ -13,7 +13,7 @@ object ext_methods:
    * Add an extension method to `Email` to retrieve the username of the email address (the part 
    * of the string before the `@` symbol).
    */
-  extension (e: Email) def username: String = ???
+  extension (e: Email) def username: String = e.value.split('@').head
 
   val sherlock = Email("sherlock@holmes.com").username
 
@@ -23,7 +23,7 @@ object ext_methods:
    * Add an extension method to `Email` to retrieve the server of the email address (the part of 
    * the string after the `@` symbol).
    */
-  // extension
+   extension (e: Email) def server: String = e.value.split('@')(2)
 
   /**
    * EXERCISE 3
@@ -31,7 +31,7 @@ object ext_methods:
    * Add an extension method to `Int` called `split` that can package up the high 16 bits and 
    * low 16 bits into a tuple of two ints, containing the two parts.
    */
-  // extension 
+   extension (i: Int) def split: (Int, Int) = (i >> 16, i & 0xFFFF) 
 
   /**
    * A rational number is one in the form n/m, where n and m are integers.
@@ -45,23 +45,39 @@ object ext_methods:
    * to `Rational`, including `+`, to add two rational numbers, `*`, to multiply two rational 
    * numbers, and `-`, to subtract one rational number from another rational number.
    */
-  // extension
+  extension (l: Rational) def +(r: Rational): Rational =
+    Rational(
+      (l.numerator * r.denominator) + (r.numerator * l.denominator),
+      l.denominator * r.denominator
+    )
+  
+  extension (l: Rational) def - (r: Rational): Rational =
+    Rational(
+      (l.numerator * r.denominator) - (r.numerator * l.denominator),
+      l.denominator * r.denominator
+    )
+  
+  extension (l: Rational) def *(r: Rational): Rational =
+    Rational(
+      l.numerator * r.numerator,
+      l.denominator * r.denominator
+    )
 
   /**
    * EXERCISE 5
    * 
    * Convert this implicit syntax class to use extension methods.
    */
-  implicit class StringOps(self: String):
-    def equalsIgnoreCase(that: String) = self.toLowerCase == that.toLowerCase
-
+  
+  extension (self: String) def equalsIgnoreCase(that: String): Boolean = self.toLowerCase == that.toLowerCase
   /**
    * EXERCISE 6
    * 
    * Import the extension method `isSherlock` into the following object so the code will compile.
    */
   object test:
-    val test: Boolean = ??? // "John Watson".isSherlock
+    import string_extensions.*
+    val test: Boolean = "John Watson".isSherlock
 
   object string_extensions:
     extension (s: String) def isSherlock: Boolean = s.startsWith("Sherlock")
@@ -74,23 +90,39 @@ object ext_methods:
    * is empty, or otherwise the head and tail in a tuple).
    */
   object list_extensions:
-    val test: Option[(String, List[String])] = ??? // List("foo", "bar").uncons
+    
+    extension[A] (l: List[A]) def uncons: Option[(A, List[A])] = l match
+      case h :: t => Some((h, t))
+      case Nil => None
 
-  /**
-   * EXERCISE 8
-   * 
-   * Add another generic extension method called `zip` to `Option[A]`, which takes an `Option[B]`, 
-   * and returns an `Option[(A, B)]`.
-   */
-  object option_extensions:
-    val test: Option[(Int, String)] = ??? // Some(123).zip(Some("foo"))
+    val test: Option[(String, List[String])] = List("foo", "bar").uncons
 
-  /**
-   * EXERCISE 9
-   * 
-   * One possible application of extension methods is adding methods to generic types of a certain
-   * shape. For example, adding `flatten` to a `List[List[A]]`, but not to other types.
-   * Add `mapInside` method to `List[Option[A]]` to map on the `A` inside the futures.
-   */
-  object list_future_extensions:
-    val digits: List[Option[Int]] = ??? // List(Some("12"), None, Some("321")).mapInside(_.length)
+        /**
+         * EXERCISE 8
+         *
+         * Add another generic extension method called `zip` to `Option[A]`, which takes an `Option[B]`, 
+         * and returns an `Option[(A, B)]`.
+         */
+        object option_extensions:
+          
+          extension[A, B] (l: Option[A]) def zip(r: Option[B]): Option[(A, B)] = (l, r) match
+            case (Some(a), Some(b)) => Some((a, b))
+            case _                  => None
+          
+          val test: Option[(Int, String)] = Some(123).zip(Some("foo"))
+
+        /**
+         * EXERCISE 9
+         *
+         * One possible application of extension methods is adding methods to generic types of a certain
+         * shape. For example, adding `flatten` to a `List[List[A]]`, but not to other types.
+         * Add `mapInside` method to `List[Option[A]]` to map on the `A` inside the futures.
+         */
+        object list_future_extensions:
+          extension[A, B] (l: List[Option[A]]) def mapInside(f: A => B): List[Option[B]] =
+            l.map{
+              case Some(a) => Some(f(a))  
+              case None => None
+            }
+          
+          val digits: List[Option[Int]] = List(Some("12"), None, Some("321")).mapInside(_.length)
